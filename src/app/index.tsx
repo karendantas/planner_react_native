2434eaf60631b4fd5ac27644fd6da7e6bc6bf895
Karen Dantas <karennobre74@gmail.com>
2024-10-26 11:38:23 -0300

    add assets, config nativewind and fonts

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+export default function Index (){
+
+    return (
+        <View className="flex-1 justify-center items-center">
+            <Text className="text-zinc-400">Hello World!</Text> 
+        </View>
+    )
+}
